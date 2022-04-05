5d2cc65861b235558e17bdc58da1cb8ed10cdaf3
gabriel-O-C <gabriel@pop-os.localdomain>
2022-04-05 14:53:25 -0300

    feat: user entity and register mutation

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,14 @@ import { buildSchema } from 'type-graphql';
 import microConfig from './mikro-orm.config';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/posts';
+import { UserResolver } from "./resolvers/user";
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   const app = express();
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
     context: () => ({ em: orm.em }) 
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { __prod__ } from "./constanst";
 import { Post } from "./entities/Post";
 import { MikroORM } from '@mikro-orm/core';
 import path from "path";
+import { User } from "./entities/User";
 export default {
     dbName: 'reddit_clone',
     migrations: {
@@ -9,7 +10,7 @@ export default {
     glob: '!(*.d).{js,ts}',
     },
     allowGlobalContext: true,
-    entities: [Post],
+    entities: [Post, User],
     debug: !__prod__,
     type: 'postgresql',
     user: 'root',
